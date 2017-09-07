896538091dda0e84be7f3f33592ef23f24b94589
NARUTOne <wznaruto326@163.com>
2017-09-07 23:00:52 +0800

    add web-app red-pack :fire:

---
+++ webApp/red-pack/blur.js
@@ -0,0 +1,15 @@
+/**
+ * canvas 微信虚图 模拟
+ * @author NARUTOne
+ * @date 2017/09/07
+ */
+
+var W = 800;
+var H = 600;
+
+var canvas = document.getElementById('canvas');
+
+var context = canvas.getContext('2d');
+
+canvas.width = W;
+canvas.height = H;
